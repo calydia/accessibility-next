e05d6981163c6f974bd1593352dcff7f5a342ae2
Sanna Mäkinen <sanna.makinen@gmail.com>
2023-03-06 20:27:49 +0200

    Some form of random main image thingy

---
+++ components/MainImage.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+import Script from 'next/script'
+
+const randomNumber = (min: number, max: number) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+const imageSource = 'bg-' + randomNumber(1, 6);
+
+const MainImage = () => {
+  return (
+    <div className="main-image--wrapper relative w-screen h-125-px md:h-250-px lg:h-350-px">
+          <div aria-hidden="true" className={`relative w-screen h-125-px md:h-250-px lg:h-350-px bg-cover bg-center ${imageSource}`}></div>
+    </div>
+  );
+};
+
+export default MainImage;
