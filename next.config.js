4adab5226729dde91a2f3721e623942b21420b8e
Sanna Mäkinen <sanna.makinen@gmail.com>
2023-03-26 17:01:50 +0300

    PWA and webp images

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const { i18n } = require('./next-i18next.config')
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
 
 const nextConfig = {
   reactStrictMode: true,
@@ -9,4 +13,4 @@ const nextConfig = {
   i18n
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
